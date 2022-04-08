273f6377de6609889ba4b858fac1645cd56d6578
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2022-04-08 19:38:18 +0100

    ED Script

---
+++ windows/MFScripts/ED.cmd
@@ -0,0 +1,18 @@
+@ECHO OFF
+call "C:\Program Files (x86)\Micro Focus\Enterprise Developer\createenv.bat"
+
+md \temp\ACCT
+cd \temp\ACCT
+xcopy "C:\Users\Public\Documents\Micro Focus\Enterprise Developer\Samples\Mainframe\CICS\Classic\ACCT" \temp\ACCT /E /H /C /I
+curl -O https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/windows/MFScripts/ACCT.xml
+mfds -g 5 \temp\ACCT\ACCT.xml
+
+md \temp\JCL
+cd \temp\JCL
+md \temp\JCL\system
+md \temp\JCL\dataset
+md \temp\JCL\loadlib
+curl -O https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/windows/MFScripts/JCL.xml
+mfds -g 5 \temp\JCL\JCL.xml
+
+cd \temp
