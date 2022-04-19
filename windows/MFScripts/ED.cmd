6f52e096bbae91ab043666af000403ff2ce60265
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2022-04-19 10:11:07 +0100

    Add BankDemo setup

--- windows/MFScripts/ED.cmd
+++ windows/MFScripts/ED.cmd
@@ -15,4 +15,11 @@ md \temp\JCL\loadlib
 curl -O https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/windows/MFScripts/JCL.xml
 mfds -g 5 \temp\JCL\JCL.xml
 
+md \MFETDUSER
+cd \MFETDUSER
+curl -O https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/windows/MFScripts/BankDemo.zip
+powershell -command "Expand-Archive -Force 'BankDemo.zip' 'BankDemo'"
+cd \MFETDUSER\BankDemo
+mfds -g 5 \MFETDUSER\BankDemo\BANKDEMO.xml
+
 cd \temp
