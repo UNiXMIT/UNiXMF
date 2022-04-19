83c562313c0062e5e0073eab1a5dda0447fd718b
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2022-04-19 10:17:20 +0100

    Updates

--- windows/MFScripts/ED.cmd
+++ windows/MFScripts/ED.cmd
@@ -1,12 +1,14 @@
 @ECHO OFF
 call "C:\Program Files (x86)\Micro Focus\Enterprise Developer\createenv.bat"
 
+:: Setup ACCT Demo Project and ES Region
 md \temp\ACCT
 cd \temp\ACCT
 xcopy "C:\Users\Public\Documents\Micro Focus\Enterprise Developer\Samples\Mainframe\CICS\Classic\ACCT" \temp\ACCT /E /H /C /I
 curl -O https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/windows/MFScripts/ACCT.xml
 mfds -g 5 \temp\ACCT\ACCT.xml
 
+:: Setup JCL Diretories and ES Region
 md \temp\JCL
 cd \temp\JCL
 md \temp\JCL\system
@@ -15,6 +17,7 @@ md \temp\JCL\loadlib
 curl -O https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/windows/MFScripts/JCL.xml
 mfds -g 5 \temp\JCL\JCL.xml
 
+:: Setup BankDemo Project and ES Region
 md \MFETDUSER
 cd \MFETDUSER
 curl -O https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/windows/MFScripts/BankDemo.zip
