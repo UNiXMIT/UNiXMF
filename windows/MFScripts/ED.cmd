29ba3fc93e090b4d03806f4a4287f9d779a1ec85
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2022-04-20 10:26:32 +0100

    Updates

--- windows/MFScripts/ED.cmd
+++ windows/MFScripts/ED.cmd
@@ -11,6 +11,9 @@ mfds -g 5 \tutorials\ACCT\ACCT.xml
 
 :: Setup JCL Demo Project and ES Region
 cd \tutorials
+md \tutorials\system
+md \tutorials\dataset
+md \tutorials\loadlib
 curl -O https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/windows/MFScripts/JCL.zip
 powershell -command "Expand-Archive -Force 'JCL.zip' 'JCL'"
 cd \tutorials\JCL
