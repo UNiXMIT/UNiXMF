574871f8bf3e7aa9f993cd9324fe7c48936939a9
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2022-04-20 10:08:36 +0100

    Updates

--- windows/MFScripts/ED.cmd
+++ windows/MFScripts/ED.cmd
@@ -1,28 +1,27 @@
 @ECHO OFF
 call "C:\Program Files (x86)\Micro Focus\Enterprise Developer\createenv.bat"
+md \tutorials
 
 :: Setup ACCT Demo Project and ES Region
-md \temp\ACCT
-cd \temp\ACCT
-xcopy "C:\Users\Public\Documents\Micro Focus\Enterprise Developer\Samples\Mainframe\CICS\Classic\ACCT" \temp\ACCT /E /H /C /I
+md \tutorials\ACCT
+cd \tutorials\ACCT
+xcopy "C:\Users\Public\Documents\Micro Focus\Enterprise Developer\Samples\Mainframe\CICS\Classic\ACCT" \tutorials\ACCT /E /H /C /I
 curl -O https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/windows/MFScripts/ACCT.xml
-mfds -g 5 \temp\ACCT\ACCT.xml
+mfds -g 5 \tutorials\ACCT\ACCT.xml
 
-:: Setup JCL Directories and ES Region
-md \temp\JCL
-cd \temp\JCL
-md \temp\JCL\system
-md \temp\JCL\dataset
-md \temp\JCL\loadlib
+:: Setup JCL Demo Project and ES Region
+cd \tutorials
+curl -O https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/windows/MFScripts/JCL.zip
+powershell -command "Expand-Archive -Force 'JCL.zip' 'JCL'"
+cd \tutorials\JCL
 curl -O https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/windows/MFScripts/JCL.xml
-mfds -g 5 \temp\JCL\JCL.xml
+mfds -g 5 \tutorials\JCL\JCL.xml
 
 :: Setup BankDemo Project and ES Region
-md \MFETDUSER
-cd \MFETDUSER
+cd \tutorials
 curl -O https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/windows/MFScripts/BankDemo.zip
 powershell -command "Expand-Archive -Force 'BankDemo.zip' 'BankDemo'"
-cd \MFETDUSER\BankDemo
-mfds -g 5 \MFETDUSER\BankDemo\BANKDEMO.xml
+cd \tutorials\BankDemo
+mfds -g 5 \tutorials\BankDemo\BANKDEMO.xml
 
-cd \temp
+cd \tutorials
