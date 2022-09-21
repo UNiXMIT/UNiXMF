1b688b777eb79f367008faca9eed95a03e65ddc9
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2022-09-21 14:40:17 +0100

    Update Linux scripts

--- MFScripts/linux/setup.sh
+++ MFScripts/linux/setup.sh
@@ -4,24 +4,17 @@
 # curl -s https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/MFScripts/linux/setup.sh | bash 
 
 user=support
-MFP=EnterpriseDeveloper
-# MFP=VisualCOBOL
-FSNAME=FSSERVER
-. /opt/microfocus/$MFP/bin/cobsetenv
+. /opt/microfocus/EnterpriseDeveloper/bin/cobsetenv
 
-cd /home/$user
-tmux new -s mfds /opt/microfocus/$MFP/bin/escwa --BasicConfig.MfRequestedEndpoint="tcp:*:10086" --write=true
-tmux new -s mfds /opt/microfocus/$MFP/bin/mfds -d
-tmux new -s fs /opt/microfocus/$MFP/bin/fs -s $FSNAME
+cd /home/$user/MFSupport
 mkdir tutorials
 
 # Setup JCL Demo Project and ES Region
-cd /home/$user/tutorials
-curl -O https://github.com/UNiXMIT/UNiXMF/raw/main/MFScripts/linux/JCL.tar.gz
-tar -zxf JCL.tar.gz
-cd /tutorials/JCL
+cd /home/$user/MFSupport/tutorials
+mkdir JCL
+cd JCL
+mkdir mkdir catalog dataset loadlib system
 curl -O https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/MFScripts/linux/JCL.xml
-mfds -g 5 /tutorials/JCL/JCL.xml
+mfds -g 5 /home/$user/MFSupport/tutorials/JCL/JCL.xml
 
-cd /home/$user/tutorials
-del JCL.tar.gz
+cd /home/$user/MFSupport
---
+++ MFScripts/linux/startmf.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Setup Intructions
+# sudo (apt/yum/zypper) install -y tmux
+. /opt/microfocus/EnterpriseDeveloper/bin/cobsetenv
+
+tmux new -s mfds /opt/microfocus/EnterpriseDeveloper/bin/escwa --BasicConfig.MfRequestedEndpoint="tcp:*:10086" --write=true
+sleep 5
+tmux new -s mfds /opt/microfocus/EnterpriseDeveloper/bin/mfds -d
+sleep 5
+tmux new -s fs /opt/microfocus/EnterpriseDeveloper/bin/fs -s FSSERVER
+sleep 5
+tmux ls
