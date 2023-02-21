055cb2a092fe7a63818f7b8574c0450a47578348
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2023-02-21 14:05:42 +0000

    Add variables to MFDBFH.cfg

--- MFScripts/windows/MFDBFH.cmd
+++ MFScripts/windows/MFDBFH.cmd
@@ -1,19 +1,28 @@
 @ECHO OFF
 
+set FILENAME=MFDBFH.cfg
+set SERVERNAME=MYSERVER
+set DRIVERNAME="{ODBC Driver 17 for SQL Server}"
+set HOST=127.0.0.1
+set MASTERDB=master
+set DATASTOREDB=support
+set USERID=support
+set PASSWD=YourStrongPassword
+
 :: Create the MFDBFH.cfg
-del MFDBFH.cfg
-dbfhconfig -add -file:MFDBFH.cfg -server:MYSERVER -provider:ss
-dbfhconfig -add -file:MFDBFH.cfg -server:MYSERVER -dsn:SS.MASTER -type:database -name:master -connect:Driver="{ODBC Driver 17 for SQL Server}";Server=127.0.0.1;Database=master;UID=support;PWD=YourStrongPassword;
-dbfhconfig -add -file:MFDBFH.cfg -server:MYSERVER -dsn:SS.SUPPORT -type:datastore -name:support -connect:Driver="{ODBC Driver 17 for SQL Server}";Server=127.0.0.1;Database=support;UID=support;PWD=YourStrongPassword;
+del %FILENAME%
+dbfhconfig -add -file:%FILENAME% -server:%SERVERNAME% -provider:ss
+dbfhconfig -add -file:%FILENAME% -server:%SERVERNAME% -dsn:SS.MASTER -type:database -name:%MASTERDB% -connect:Driver=%DRIVERNAME%;Server=%HOST%;Database=%MASTERDB%;UID=%USERID%;PWD=%PASSWD%;
+dbfhconfig -add -file:%FILENAME% -server:%SERVERNAME% -dsn:SS.DATASTORE -type:datastore -name:%DATASTOREDB% -connect:Driver=%DRIVERNAME%;Server=%HOST%;Database=%DATASTOREDB%;UID=%USERID%;PWD=%PASSWD%;
 
 :: Create the datastore
-dbfhdeploy data create sql://MYSERVER/SUPPORT
+dbfhdeploy data create sql://%SERVERNAME%/%DATASTOREDB%
 
 :: ES Region Environment Variables
 :: [ES-ENVIRONMENT]
 :: ES_DB_FH=Y
 :: ES_DB_SERVER=MYSERVER
-:: MFDBFH_CONFIG=C:\mfdbfh.cfg
+:: MFDBFH_CONFIG=C:\MFDBFH.cfg
 
 :: JES -> Configuration -> Default Allocated Dataset Location
 :: sql://MYSERVER/SUPPORT?type=folder;folder=/DATA
