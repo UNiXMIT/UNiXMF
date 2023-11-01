eb597b78fc61e916dc66d38798ee6c2aebbe0511
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2023-11-01 11:09:21 +0000

    Combine scripts

--- windows/MFScripts/AutoPAC.cmd
+++ windows/MFScripts/AutoPAC.cmd
@@ -13,6 +13,16 @@ if %errorLevel% == 0 (
 )
 
 CALL "C:\Program Files (x86)\Micro Focus\Enterprise Developer\createenv.bat"
+
+IF %1==-r GOTO :REMOVEAUTOPAC
+IF %1==-R GOTO :REMOVEAUTOPAC
+IF %1==/r GOTO :REMOVEAUTOPAC
+IF %1==/R GOTO :REMOVEAUTOPAC
+IF %1==-h GOTO :USAGE
+IF %1==-H GOTO :USAGE
+IF %1==/h GOTO :USAGE
+IF %1==/H GOTO :USAGE
+
 if not exist \MFSamples md \MFSamples
 cacls \MFSamples /e /p Everyone:f
 
@@ -83,5 +93,34 @@ timeout /T 5
 :: Cold start regions
 casstart /rREGION1 /s:c
 casstart /rREGION2 /s:c
+GOTO :END
+
+:REMOVEAUTOPAC
+TASKKILL -F -IM cassi.exe & TASKKILL -F -IM casmgr.exe & TASKKILL -F -IM castsc.exe & TASKKILL -F -IM cascd.exe & TASKKILL -F -IM MFCS.EXE & TASKKILL -F -IM CASTRC.EXE & TASKKILL -F -IM CASDBC.EXE & TASKKILL -F -IM CASTMC.EXE & TASKKILL -F -IM CASMQB.EXE & TASKKILL -F -IM CASPRT.EXE
+
+:: ESCWA - Remove SOR, PAC and PAC Regions
+FOR /F "tokens=* USEBACKQ" %%g IN (`curl -s -X "GET" "http://localhost:10086/server/v1/config/groups/sors" -H "accept: application/json" -H "X-Requested-With: API" -H "Origin: http://localhost:10086" ^| jq -r .[0].Uid`) do (SET "SORUID=%%g")
+
+curl -s -X "DELETE" "http://localhost:10086/server/v1/config/groups/sors/%SORUID%" -H "accept: application/json" -H "X-Requested-With: API" -H "Content-Type: application/json" -H "Origin: http://localhost:10086"
+
+FOR /F "tokens=* USEBACKQ" %%g IN (`curl -s -X "GET" "http://localhost:10086/server/v1/config/groups/pacs" -H "accept: application/json" -H "X-Requested-With: API" -H "Origin: http://localhost:10086" ^| jq -r .[0].Uid`) do (SET "PACUID=%%g")
+
+curl -s -X "DELETE" "http://localhost:10086/native/v1/regions/127.0.0.1/86/REGION1" -H "accept: application/json" -H "X-Requested-With: API" -H "Content-Type: application/json" -H "Origin: http://localhost:10086"
+
+curl -s -X "DELETE" "http://localhost:10086/native/v1/regions/127.0.0.1/86/REGION2" -H "accept: application/json" -H "X-Requested-With: API" -H "Content-Type: application/json" -H "Origin: http://localhost:10086"
+
+:: Remove files
+rd \MFSamples\PAC /Q /S
+GOTO :END
+
+:USAGE
+ECHO Options:
+ECHO  AutoPac                        Setup AutoPAC in ES
+ECHO  AutoPac options                Remove AutoPAC or display script usage
+ECHO.    
+ECHO Usage: 
+ECHO  -r              Remove AutoPAC from ES
+ECHO  -h              Usage
+GOTO :END
 
 :END
