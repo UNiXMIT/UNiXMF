0163b4a114b9b174284052244126bef01dd9e627
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2023-11-07 16:59:30 +0000

    formatdumps

---
+++ linux/MFScripts/formatdumps.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# curl -s https://raw.githubusercontent.com/UNiXMIT/UNiXMF/main/linux/MFScripts/formatdumps.sh | bash 
+
+if [[ -f "casdumpa.rec" ]]; then
+    printf "\nFormatting casdumpa...\n"
+    casdup$1 -icasdumpa.rec -fcasdumpa.txt -w -d
+fi
+if [[ -f "casdumpb.rec" ]]; then
+    printf "\nFormatting casdumpb...\n"
+    casdup$1 -icasdumpb.rec -fcasdumpb.txt -w -d
+fi
+if [[ -f "casdumpx.rec" ]]; then
+    printf "\nFormatting casdumpx...\n"
+    casdup$1 -icasdumpx.rec -fcasdumpx.txt -w -d
+fi
+if [[ -f "casauxta.rec" ]]; then
+    printf "\nFormatting casauxta...\n"
+    casdup$1 -icasauxta.rec -fcasauxta.txt -w -d
+fi
+if [[ -f "casauxtb.rec" ]]; then
+    printf "\nFormatting casauxtb...\n"
+    casdup$1 -icasauxtb.rec -fcasauxtb.txt -w -d
+fi
+printf "\nFormatting Complete!\n"
