c6d075ac7e4580aad3fab5cb3354cd9d48710816
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2023-11-08 10:06:04 +0000

    Improve formatdumps

--- linux/MFScripts/formatdumps.sh
+++ linux/MFScripts/formatdumps.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
+DUMPS=0
 if [[ -f "casdumpa.rec" ]]; then
+    DUMPS=1
     printf "\nFormatting casdumpa...\n"
     casdup$1 -icasdumpa.rec -fcasdumpa.txt -w -d
 fi
 if [[ -f "casdumpb.rec" ]]; then
+    DUMPS=1
     printf "\nFormatting casdumpb...\n"
     casdup$1 -icasdumpb.rec -fcasdumpb.txt -w -d
 fi
 if [[ -f "casdumpx.rec" ]]; then
+    DUMPS=1
     printf "\nFormatting casdumpx...\n"
     casdup$1 -icasdumpx.rec -fcasdumpx.txt -w -d
 fi
 if [[ -f "casauxta.rec" ]]; then
+    DUMPS=1
     printf "\nFormatting casauxta...\n"
     casdup$1 -icasauxta.rec -fcasauxta.txt -w -d
 fi
 if [[ -f "casauxtb.rec" ]]; then
+    DUMPS=1
     printf "\nFormatting casauxtb...\n"
     casdup$1 -icasauxtb.rec -fcasauxtb.txt -w -d
 fi
-printf "\nFormatting Complete!\n"
+if [[ $DUMPS = 1 ]]; then
+    printf "\nFormatting Complete!\n\n"
+elif [[ $DUMPS = 0 ]]; then
+    printf "\nNo Files Found!\n\n"
+fi
--- windows/MFScripts/FormatDumps.cmd
+++ windows/MFScripts/FormatDumps.cmd
@@ -1,23 +1,33 @@
 @ECHO OFF
 CALL "C:\Program Files (x86)\Micro Focus\Enterprise Developer\createenv.bat"
+set DUMPS=0
 IF EXIST casdumpa.rec (
+    set DUMPS=1
     ECHO Formatting casdumpa...
     casdup /icasdumpa.rec /fcasdumpa.txt /w /d
 )
 IF EXIST casdumpb.rec (
+    set DUMPS=1
     ECHO Formatting casdumpb...
     casdup /icasdumpb.rec /fcasdumpb.txt /w /d
 )
 IF EXIST casdumpx.rec (
+    set DUMPS=1
     ECHO Formatting casdumpx...
     casdup /icasdumpx.rec /fcasdumpx.txt /w /d
 )
 IF EXIST casauxta.rec (
+    set DUMPS=1
     ECHO Formatting casauxta...
     casdup /icasauxta.rec /fcasauxta.txt /w /d
 )
 IF EXIST casauxtb.rec (
+    set DUMPS=1
     ECHO Formatting casauxtb...
     casdup /icasauxtb.rec /fcasauxtb.txt /w /d
 )
-ECHO Formatting Complete!
+IF %DUMPS%==1 (
+    ECHO Formatting Complete!
+) ELSE IF %DUMPS%==0 (
+    ECHO No Files Found!
+)
