9b86fdda661c7abc50fa9165305850d3febabe5c
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2023-12-12 10:38:06 +0000

    Cron job

--- linux/openldap/newpasswd.sh
+++ linux/openldap/newpasswd.sh
@@ -5,4 +5,5 @@ if [[ -n "${SLAPPWD}" ]]; then
     sed -i "/olcRootPW:.*/c\olcRootPW: $SECRET" /openldap/schema/newpasswd.ldif
     /usr/sbin/slapd -u ldap -h "ldap:/// ldaps:/// ldapi:///"
     ldapmodify -H ldapi:// -Y EXTERNAL -f /openldap/schema/newpasswd.ldif
+    crontab -r
 fi
