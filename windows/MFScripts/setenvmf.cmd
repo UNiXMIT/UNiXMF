9516c3cee8351db6c279e125c7ff5fe9d4f89e4a
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2024-01-24 09:08:00 +0000

    Improve setenvmf for all products

--- windows/MFScripts/setenvmf.cmd
+++ windows/MFScripts/setenvmf.cmd
@@ -1,4 +1,7 @@
 @ECHO OFF
 set MYDIR=%CD%
-call "C:\Program Files (x86)\Micro Focus\Enterprise Developer\createenv.bat" %1
+IF EXIST "C:\Program Files (x86)\Micro Focus\Enterprise Developer" call "C:\Program Files (x86)\Micro Focus\Enterprise Developer\createenv.bat" %1
+IF EXIST "C:\Program Files (x86)\Micro Focus\Enterprise Server" call "C:\Program Files (x86)\Micro Focus\Enterprise Server\createenv.bat" %1
+IF EXIST "C:\Program Files (x86)\Micro Focus\Visual COBOL" call "C:\Program Files (x86)\Micro Focus\Visual COBOL\createenv.bat" %1
+IF EXIST "C:\Program Files (x86)\Micro Focus\COBOL Server" call "C:\Program Files (x86)\Micro Focus\COBOL Server\createenv.bat" %1
 cd %MYDIR%
