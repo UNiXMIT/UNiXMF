c35b9b283172094059aa043a055c03b631078fe8
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2024-08-01 16:33:11 +0100

    Create entrypoint.sh

--- linux/openldap/entrypoint.sh
+++ linux/openldap/entrypoint.sh
@@ -1,9 +1,3 @@
-#!bin/bash
-if [[ -n "${SLAPPWD}" ]]; then
-    SECRET=$(slappasswd -s ${SLAPPWD})
-    pkill slapd
-    sed -i "/olcRootPW:.*/c\olcRootPW: $SECRET" /openldap/schema/newpasswd.ldif
-    /usr/sbin/slapd -u ldap -h "ldap:/// ldaps:/// ldapi:///"
-    ldapmodify -H ldapi:// -Y EXTERNAL -f /openldap/schema/newpasswd.ldif
-fi
-/sbin/init
+#!/bin/bash
+/openldap/newpasswd.sh
+exec "$@"
