ce0439825dfa8c138a8da31db8857bc6b7d465a9
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2025-06-09 09:17:07 +0100

    Add RS path to script

--- windows/MFScripts/setenvmf.cmd
+++ windows/MFScripts/setenvmf.cmd
@@ -1,5 +1,9 @@
 @ECHO OFF
 set MYDIR=%CD%
+IF EXIST "C:\Program Files (x86)\Rocket Software\Enterprise Developer" CALL "C:\Program Files (x86)\Rocket Software\Enterprise Developer\createenv.bat" %1
+IF EXIST "C:\Program Files (x86)\Rocket Software\Enterprise Server" CALL "C:\Program Files (x86)\Rocket Software\Enterprise Server\createenv.bat" %1
+IF EXIST "C:\Program Files (x86)\Rocket Software\Visual COBOL" CALL "C:\Program Files (x86)\Rocket Software\Visual COBOL\createenv.bat" %1
+IF EXIST "C:\Program Files (x86)\Rocket Software\COBOL Server" CALL "C:\Program Files (x86)\Rocket Software\COBOL Server\createenv.bat" %1
 IF EXIST "C:\Program Files (x86)\Micro Focus\Enterprise Developer" CALL "C:\Program Files (x86)\Micro Focus\Enterprise Developer\createenv.bat" %1
 IF EXIST "C:\Program Files (x86)\Micro Focus\Enterprise Server" CALL "C:\Program Files (x86)\Micro Focus\Enterprise Server\createenv.bat" %1
 IF EXIST "C:\Program Files (x86)\Micro Focus\Visual COBOL" CALL "C:\Program Files (x86)\Micro Focus\Visual COBOL\createenv.bat" %1
