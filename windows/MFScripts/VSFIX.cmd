f48ff9c5b51d1eac46cab282d3498f4bc2e4931f
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2025-06-11 10:12:40 +0100

    Improve script

--- windows/MFScripts/VSFIX.cmd
+++ windows/MFScripts/VSFIX.cmd
@@ -1,7 +1,27 @@
 @ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-SET "VSBASE=C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE\"
+SET "VSWHERE=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+
+IF NOT EXIST "%VSWHERE%" (
+    ECHO vswhere.exe not found
+    EXIT /b 1
+)
+
+FOR /F "USEBACKQ TOKENS=*" %%I IN (`"%VSWHERE%" -latest -property installationPath`) do (
+    set "VSInstallPath=%%I"
+)
+
+FOR /F "USEBACKQ TOKENS=*" %%X IN (`"%VSWHERE%" -latest -property displayName`) do (
+    set "VSName=%%X"
+)
+
+if "%VSInstallPath%"=="" (
+    echo Visual Studio not found
+    exit /b 1
+)
+
+set "VSBASE=%VSInstallPath%\Common7\IDE\"
 CD "%VSBASE%Extensions"
 SET "PKGDEF=CBLPRJ.PKGDEF"
 SET "foundPath="
@@ -34,5 +54,11 @@ FINDSTR /V "%stringToRemove1%" "%inputFile%" > "%tempFile%"
 
 MOVE /y "%tempFile%" "%inputFile%" >nul
 ECHO Lines removed successfully. Output saved to "%inputFile%"
+ECHO Updating %VSName% configuration...
 "%VSBASE%devenv.exe" /UPDATECONFIGURATION
+IF ERRORLEVEL 1 (
+    ECHO Failed to update %VSName% configuration!
+    EXIT /b 1
+)
+ECHO %VSName% configuration update completed successfully.
 ENDLOCAL
