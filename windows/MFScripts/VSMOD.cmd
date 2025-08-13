a95ebe07fa5ffa2f9c04bc310cafd41ff1678bb5
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2025-08-13 08:12:12 +0100

    Add VSMOD.cmd script for Visual Studio component modifications

---
+++ windows/MFScripts/VSMOD.cmd
@@ -0,0 +1,16 @@
+@ECHO OFF
+
+C:\Users\Public\Documents\vs_professional.exe modify --wait --passive --norestart --downloadThenInstall ^
+--add Microsoft.Net.Component.4.5.TargetingPack ^
+--add Microsoft.Net.Component.4.5.2.TargetingPack ^
+--add Microsoft.VisualStudio.Component.Debugger.JustInTime ^
+--add Microsoft.VisualStudio.Component.GraphDocument ^
+--add Microsoft.VisualStudio.Component.NuGet ^
+--add Microsoft.VisualStudio.Component.DockerTools ^
+--add Microsoft.VisualStudio.Component.VisualStudioData ^
+--add Microsoft.VisualStudio.Component.Web ^
+--add Microsoft.VisualStudio.Workload.ManagedDesktop ^
+--add Microsoft.VisualStudio.Workload.Azure ^
+--add Microsoft.VisualStudio.Component.Wcf.Tooling ^
+--add Microsoft.VisualStudio.Component.SQL.SSDT ^
+--add Microsoft.VisualStudio.Workload.NetCoreTools
