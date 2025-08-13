5f6bb8d861dd590fc64fffb8001d13bb28111e46
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2025-08-13 12:07:26 +0100

    Refactor VSMOD.cmd to use vswhere for Visual Studio installation path detection

--- windows/MFScripts/VSMOD.cmd
+++ windows/MFScripts/VSMOD.cmd
@@ -1,18 +1,24 @@
 @ECHO OFF
+SETLOCAL ENABLEDELAYEDEXPANSION
 
-C:\Users\Public\Documents\vs_professional.exe modify --wait --passive --norestart --downloadThenInstall ^
---add Microsoft.Net.Component.4.5.TargetingPack ^
---add Microsoft.Net.Component.4.5.2.TargetingPack ^
---add Microsoft.Net.Component.4.7.2.TargetingPack ^
---add Microsoft.VisualStudio.Component.Debugger.JustInTime ^
---add Microsoft.VisualStudio.Component.GraphDocument ^
---add Microsoft.VisualStudio.Component.NuGet ^
---add Microsoft.VisualStudio.Component.Windows10SDK.18362 ^
---add Microsoft.VisualStudio.Component.DockerTools ^
---add Microsoft.VisualStudio.Component.VisualStudioData ^
---add Microsoft.VisualStudio.Component.Web ^
---add Microsoft.VisualStudio.Workload.ManagedDesktop ^
---add Microsoft.VisualStudio.Workload.Azure ^
---add Microsoft.VisualStudio.Component.Wcf.Tooling ^
---add Microsoft.VisualStudio.Component.SQL.SSDT ^
---add Microsoft.VisualStudio.Workload.NetCoreTools
+SET "VSWHERE=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+
+IF NOT EXIST "%VSWHERE%" (
+    ECHO vswhere.exe not found
+    EXIT /b 1
+)
+
+FOR /F "USEBACKQ TOKENS=*" %%I IN (`"%VSWHERE%" -latest -property installationPath`) do (
+    set "VSInstallPath=%%I"
+)
+
+FOR /F "USEBACKQ TOKENS=*" %%X IN (`"%VSWHERE%" -latest -property displayName`) do (
+    set "VSName=%%X"
+)
+
+if "%VSInstallPath%"=="" (
+    echo Visual Studio not found
+    exit /b 1
+)
+
+"C:\Program Files (x86)\Microsoft Visual Studio\Installer\setup" modify --passive --norestart --downloadThenInstall --installPath "%VSInstallPath%" --add Microsoft.Net.Component.4.5.TargetingPack --add Microsoft.Net.Component.4.5.2.TargetingPack --add Microsoft.Net.Component.4.7.2.TargetingPack --add Microsoft.VisualStudio.Component.Debugger.JustInTime --add Microsoft.VisualStudio.Component.GraphDocument --add Microsoft.VisualStudio.Component.NuGet --add Microsoft.VisualStudio.Component.Windows10SDK.18362 --add Microsoft.VisualStudio.Component.DockerTools --add Microsoft.VisualStudio.Component.VisualStudioData --add Microsoft.VisualStudio.Component.Web --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.Azure --add Microsoft.VisualStudio.Component.Wcf.Tooling --add Microsoft.VisualStudio.Component.SQL.SSDT --add Microsoft.VisualStudio.Workload.NetCoreTools
