7da0e5377b958cae8e59c203b9f0df01c36a9cc2
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2025-08-13 08:13:32 +0100

    Add missing Visual Studio components to VSMOD.cmd script

--- windows/MFScripts/VSMOD.cmd
+++ windows/MFScripts/VSMOD.cmd
@@ -3,9 +3,11 @@
 C:\Users\Public\Documents\vs_professional.exe modify --wait --passive --norestart --downloadThenInstall ^
 --add Microsoft.Net.Component.4.5.TargetingPack ^
 --add Microsoft.Net.Component.4.5.2.TargetingPack ^
+--add Microsoft.Net.Component.4.7.2.TargetingPack ^
 --add Microsoft.VisualStudio.Component.Debugger.JustInTime ^
 --add Microsoft.VisualStudio.Component.GraphDocument ^
 --add Microsoft.VisualStudio.Component.NuGet ^
+--add Microsoft.VisualStudio.Component.Windows10SDK.18362 ^
 --add Microsoft.VisualStudio.Component.DockerTools ^
 --add Microsoft.VisualStudio.Component.VisualStudioData ^
 --add Microsoft.VisualStudio.Component.Web ^
