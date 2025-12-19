9093fa74d6e5e5a59c089833324b7df3fc028733
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2025-12-19 20:12:28 +0000

    Refactor setenvmf.sh to simplify MFCOBOL installation selection and remove redundant code

--- linux/MFScripts/setenvmf.sh
+++ linux/MFScripts/setenvmf.sh
@@ -1,29 +1,17 @@
 #!/bin/bash
-# The startmf.sh/setupmf.sh scripts and services, run after this to start/stop services, will use the environment set here.
+export MFDIR=/home/products
 
-# MF_OPT is the default location of all your MFCOBOL installations
-export MF_OPT=/home/products
+CURRENT_DIR=$(pwd)
 
-set_mf()
-{
-    # Save current directory.
-    CURRENT_DIR=$(pwd)
+cd $MFDIR
+array=(*/)
 
-    cd $MF_OPT
-    array=(*/)
+echo
+PS3="Enter the NUMBER of the MFCOBOL installation to use: "
+select MF in "${array[@]}"
+do export MFCOBOL=$(echo $MFDIR/$MF | sed -e "s/\/*$//"); break;  done
 
-    # Display a list of all MF installations.
-    echo
-    PS3="Enter the NUMBER of the MFCOBOL installation to use: "
-    select MF in "${array[@]}"
-    do export MFCOBOL=$(echo $MF_OPT/$MF | sed -e "s/\/*$//"); break;  done
+cd $CURRENT_DIR
 
-    # Change directory back to original.
-    cd $CURRENT_DIR
-
-    # Set environment.
-    . $MFCOBOL/bin/cobsetenv
-    echo "export MFCOBOL=\"$MFCOBOL\"" > /tmp/.setenvmf
-}
-
-set_mf
+. $MFCOBOL/bin/cobsetenv
+echo "export MFCOBOL=\"$MFCOBOL\"" > /tmp/.setenvmf
