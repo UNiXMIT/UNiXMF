79db4070c91ab00cb12097d6cb8416fec71ca486
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2026-05-05 12:09:02 +0100

    Update Visual Studio components in VSMOD.cmd to latest versions and improve readability

--- windows/MFScripts/VSMOD.cmd
+++ windows/MFScripts/VSMOD.cmd
@@ -21,4 +21,17 @@ if "%VSInstallPath%"=="" (
     exit /b 1
 )
 
-"C:\Program Files (x86)\Microsoft Visual Studio\Installer\setup" modify --passive --norestart --downloadThenInstall --installPath "%VSInstallPath%" --add Microsoft.Net.Component.4.5.TargetingPack --add Microsoft.Net.Component.4.5.2.TargetingPack --add Microsoft.Net.Component.4.7.2.TargetingPack --add Microsoft.VisualStudio.Component.Debugger.JustInTime --add Microsoft.VisualStudio.Component.GraphDocument --add Microsoft.VisualStudio.Component.NuGet --add Microsoft.VisualStudio.Component.Windows10SDK.18362 --add Microsoft.VisualStudio.Component.DockerTools --add Microsoft.VisualStudio.Component.VisualStudioData --add Microsoft.VisualStudio.Component.Web --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.Azure --add Microsoft.VisualStudio.Component.Wcf.Tooling --add Microsoft.VisualStudio.Component.SQL.SSDT --add Microsoft.VisualStudio.Workload.NetCoreTools
+"C:\Program Files (x86)\Microsoft Visual Studio\Installer\setup" modify --passive --norestart --downloadThenInstall --installPath "%VSInstallPath%" ^
+--add Microsoft.Net.Component.4.8.1.TargetingPack ^
+--add Microsoft.VisualStudio.Component.Debugger.JustInTime ^
+--add Microsoft.VisualStudio.Component.GraphDocument ^
+--add Microsoft.VisualStudio.Component.NuGet ^
+--add Microsoft.VisualStudio.Component.DockerTools ^
+--add Microsoft.VisualStudio.Component.VisualStudioData ^
+--add Microsoft.VisualStudio.Component.Web ^
+--add Microsoft.VisualStudio.Workload.ManagedDesktop ^
+--add Microsoft.VisualStudio.Workload.Azure ^
+--add Microsoft.VisualStudio.Component.Wcf.Tooling ^
+--add Microsoft.VisualStudio.Component.SQL.SSDT ^
+--add Microsoft.VisualStudio.Workload.NetCoreTools ^
+--add Microsoft.VisualStudio.Component.Windows10SDK
