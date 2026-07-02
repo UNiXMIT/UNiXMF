914a6884bcad1f5f4189e1c8feb82cfa13ce228b
Martin Turner <mturner@rocketsoftware.com>
2026-07-02 16:51:28 +0100

    Refactor setenvmf.sh for improved readability and error handling in MFCOBOL selection

--- linux/MFScripts/setenvmf.sh
+++ linux/MFScripts/setenvmf.sh
@@ -1,22 +1,45 @@
 #!/bin/bash
+
 export MFDIR=/home/products
 export COBMODE=64
 export TERM=xterm
 
 CURRENT_DIR=$(pwd)
 
-cd $MFDIR
+cd "$MFDIR" || exit 1
 array=(*/)
 
 echo
-PS3="Enter the NUMBER of the MFCOBOL installation to use: "
-select MF in "${array[@]}"
-do export MFCOBOL=$(echo $MFDIR/$MF | sed -e "s/\/*$//"); break;  done
 
-cd $CURRENT_DIR
+if [[ -n "$1" ]]; then
+    # Non-interactive mode
+    index=$(( $1 - 1 ))
 
-. $MFCOBOL/bin/cobsetenv
-rm -f /tmp/.mf.env
-echo "export MFCOBOL=\"$MFCOBOL\"" > /tmp/.mf.env
-echo "export COBMODE=\"$COBMODE\"" >> /tmp/.mf.env
-echo "export TERM=\"$TERM\"" >> /tmp/.mf.env
+    if (( index < 0 || index >= ${#array[@]} )); then
+        echo "Invalid selection: $1"
+        echo "Valid selections are 1-${#array[@]}"
+        exit 1
+    fi
+
+    MF="${array[$index]}"
+else
+    # Interactive mode
+    PS3="Enter the NUMBER of the MFCOBOL installation to use: "
+    select MF in "${array[@]}"; do
+        [[ -n "$MF" ]] && break
+        echo "Invalid selection"
+    done
+fi
+
+export MFCOBOL="${MFDIR}/${MF%/}"
+
+cd "$CURRENT_DIR" || exit 1
+
+set --
+. "$MFCOBOL/bin/cobsetenv"
+
+cat > /tmp/.mf.env <<EOF
+export MFCOBOL="$MFCOBOL"
+export COBMODE="$COBMODE"
+export TERM="$TERM"
+EOF
